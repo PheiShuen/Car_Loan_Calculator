7d1d516c4098a656e4df3d04b7ec852e1c2ce3c1
Phei_Shuen <chuahps-wm17@student.tarc.edu.my>
2019-10-31 11:06:52 +0800

    CAR LOAN CALCULATOR

---
+++ app/src/main/java/com/example/carloancalculator/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.carloancalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonCalculate.setOnClickListener {
+            calculateLoan()
+        }
+    }
+
+    private fun calculateLoan(){
+        //getting input data from the user
+        val car_price = editTextCarPrice.text.toString().toInt()
+        val down_payment = editTextDownPayment.text.toString().toInt()
+        val loan_period = editTextLoanPeriod.text.toString().toInt()
+        val interest_rate = editTextInterestRate.text.toString().toFloat()
+
+        val loan = car_price - down_payment
+        textViewCarLoan.text = getString(R.string.car_loan)+": ${loan}"
+        val interest = loan*interest_rate*loan_period
+        textViewInterest.text=getString(R.string.interest)+": ${interest}"
+        val mPayment = (loan+interest)/loan_period/12
+        textViewMonthlyPayment.text = getString(R.string.monthly_payment)+": ${mPayment}"
+    }
+    fun resetInput(view: View){
+
+
+    }
+}
